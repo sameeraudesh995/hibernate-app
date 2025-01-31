e35690b371b7b9b675a7c34a721d8e9b23ab862a
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-01-31 18:30:32 +0530

    main form created

---
+++ src/contorller/MainFormController.java
@@ -0,0 +1,19 @@
+package contorller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class MainFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colUpdate;
+    public TableColumn colDelete;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+}
